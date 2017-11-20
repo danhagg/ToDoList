f5c6d74bdcf8f8b959abad2c40cb4fb538933393
danhagg <danielhaggerty1976@gmail.com>
2017-11-19 20:28:47 -0600

    added uuid

--- projectmanager/src/App.js
+++ projectmanager/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Projects from './Components/Projects';
 import AddProject from './Components/AddProject';
+import uuid from 'uuid';
 import './App.css';
 
 class App extends Component {
@@ -16,22 +17,27 @@ class App extends Component {
   componentWillMount () {
     this.setState({projects: [
       {
+        id: uuid.v4(),
         title: 'Business website',
         category: 'Web Design'
       },
       {
+        id: uuid.v4(),
         title: 'Social App',
         category: 'Mobile Development'
       },
       {
+        id: uuid.v4(),
         title: 'Ecommerce Shopping Cart',
         category: 'Web Development'
       }
     ]});
   }
-
+  // get state, push new project to it, set state again
   handleAddProject (project) {
-    console.log(project);
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects: projects});
   }
 
   render () {
--- projectmanager/src/Components/AddProject.js
+++ projectmanager/src/Components/AddProject.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import uuid from 'uuid';
 
 class AddProject extends Component {
   constructor() {
@@ -17,6 +18,7 @@ class AddProject extends Component {
       alert('Title is required');
     } else {
       this.setState({newProject:{
+        id: uuid.v4(),
         title: this.refs.title.value,
         category: this.refs.category.value
       }}, function () {
@@ -28,7 +30,7 @@ class AddProject extends Component {
 
   render () {
     let categoryOptions = this.props.categories.map(category => {
-      return <option key={category} value="category">{category}</option>
+      return <option key={category} value={category}>{category}</option>
     });
     return (
       <div>
@@ -44,7 +46,9 @@ class AddProject extends Component {
             {categoryOptions}
             </select>
           </div>
+          <br />
           <input type="submit" value="Submit" />
+          <br />
         </form>
       </div>
     );
